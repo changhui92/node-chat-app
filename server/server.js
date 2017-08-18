42ae1ef6136aae5be3a1d1cff9d8c60a89bd3b4c
Koh Chang Hui <changhui92@Kohs-MacBook-Pro-2.local>
2017-08-18 16:02:18 +0800

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Chang',
+    text: 'Hello, are you there?',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  })
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
