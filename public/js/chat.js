8f846fb2ad346593dce3edda9a622b5b8dd2d8bd
Koh Chang Hui <changhui92@Kohs-MacBook-Pro-2.local>
2017-08-19 12:48:27 +0800

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -94,7 +94,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextBox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: jQuery('[name=message]').val()
   }, function () {
     messageTextBox.val('');
